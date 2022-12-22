85787be7719ec74500bb04ab37ade0a5d92ca214
rabbani4u <rabbani.iu31@gmail.com>
2022-12-22 16:08:55 +0600

    update

--- src/Checkout.js
+++ src/Checkout.js
@@ -2,37 +2,45 @@ import React from "react";
 import { useStateValue } from "./StateProvider";
 import "./Checkout.css";
 import CheckoutProduct from "./CheckoutProduct";
+import Subtotal from "./Subtotal";
 
 function Checkout() {
   const [{ basket }] = useStateValue();
   return (
     <div className="checkout">
-      <img
-        className="checkout__ad"
-        src="https://img.freepik.com/free-vector/flat-horizontal-sale-banner-template-with-photo_23-2149000923.jpg?w=826&t=st=1671692361~exp=1671692961~hmac=715832a0463e58bc5b9b837bb5eac7b3fa6a284f19d6209ae619ddb0a48ac841"
-        alt=""
-      />
-      {basket?.length === 0 ? (
-        <div>
-          <h2>Your Shopping Basket is empty</h2>
-          <p>
-            There are many variations of passages of Lorem Ipsum available, but
-            the majority have suffered alteration in some form
-          </p>
-        </div>
-      ) : (
-        <div>
-          <h2 className="checkout_title">Your Shopping Basket</h2>
-          {/* List all chechout products */}
-          {basket.map(item => (
-            <CheckoutProduct
-              id={item.id}
-              image={item.image}
-              title={item.title}
-              price={item.price}
-              rating={item.rating}
-            />
-          ))}
+      <div className="checkout__left">
+        <img
+          className="checkout__ad"
+          src="https://img.freepik.com/free-vector/flat-horizontal-sale-banner-template-with-photo_23-2149000923.jpg?w=826&t=st=1671692361~exp=1671692961~hmac=715832a0463e58bc5b9b837bb5eac7b3fa6a284f19d6209ae619ddb0a48ac841"
+          alt=""
+        />
+        {basket?.length === 0 ? (
+          <div>
+            <h2>Your Shopping Basket is empty</h2>
+            <p>
+              There are many variations of passages of Lorem Ipsum available,
+              but the majority have suffered alteration in some form
+            </p>
+          </div>
+        ) : (
+          <div>
+            <h2 className="checkout_title">Your Shopping Basket</h2>
+            {/* List all chechout products */}
+            {basket.map(item => (
+              <CheckoutProduct
+                id={item.id}
+                image={item.image}
+                title={item.title}
+                price={item.price}
+                rating={item.rating}
+              />
+            ))}
+          </div>
+        )}
+      </div>
+      {basket.length > 0 && (
+        <div className="checkout_right">
+          <Subtotal />
         </div>
       )}
     </div>
---
+++ src/Subtotal.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Subtotal() {
+  return (
+    <div className="subtotal">
+      {/* price */}
+      <button>Proced to Checkout</button>
+    </div>
+  );
+}
+
+export default Subtotal;
